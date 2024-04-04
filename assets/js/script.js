c077f1d1980b347d5f2c7dfee727ec366ca794d7
Fresia Miranda <fresia.miranda@gmail.com>
2024-04-04 18:55:19 -0300

    primer commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,37 @@
-
-$(window.scroll(function)){
-    if (($"#menu").offset().top> 900){
-      $("#menu").addClass("bg-black");
-    } else{
-      $("#menu").removeClass("bg-black")
+$(document).ready(function () {
+    
+  //cambiar a fondo negro cuando esté fuera del header
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > $('header').outerHeight()) {
+      alert($('header').outerHeight());
+      $('nav').addClass('bg-black');
+    } else {
+      $('nav').removeClass('bg-black');
     }
-}
+  });
+  
+  //Aplicar Smooth scroll al pinchar en los links
+  $("a.link").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+
+        window.location.hash = hash;
+      });
+    }
+  });
+  /*
+  mostrar alerta y ocultarlo después de 4 segundos
+  */
+  $("#envioC").on("click", function () {
+    $("#alert").addClass("d-block");
+    $("#alert").removeClass("d-none");
+    setTimeout(function () {
+      $("#alert").addClass("d-none");
+      $("#alert").removeClass("d-block");
+    }, 4000);
+  });
+});
